40d40a19166d5d5f2e5da0ec441658d26ba0afd4
Legates1 <103452467+Legates1@users.noreply.github.com>
2022-06-09 00:58:10 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+
+function render(leads) {
+    
+    let listItems = ""
+    for (var i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+                <button id="dlt-btn" ' >DELETE</button>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+    let test1 = document.getElementById("dlt-btn")
+    if (listItems){
+    test1.addEventListener("click",function(i){
+        myLeads.splice(i,1)
+        render(myLeads)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    })
+    }
+}
+
+
+
+
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    if (inputEl.value == ""){
+        return
+    }
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
